04ede6d41117f0c0a3967cdd90cb792a9d4e3f56
lfreyes97 <reyesdelosreyes.1997@gmail.com>
2022-02-12 02:03:27 -0500

    Update Navbar and add fonts

--- components/Header.vue
+++ components/Header.vue
@@ -1,20 +1,39 @@
 <template>
-<header class="h-24 sm:h-32 flex items-center z-30 w-full">
-        <div class="container mx-auto px-6 flex items-center justify-between">
-            <div class="uppercase text-gray-800 dark:text-white font-black text-3xl">
-                <h1>
-                Prof. Juan C. Reyes
-                </h1>
-            </div>
-            <div class="flex items-center">
-                <nav class="font-sen text-gray-800 dark:text-white uppercase text-lg lg:flex items-center hidden">
-                    <nuxt-link to="/" class="py-2 px-6 flex hover:text-blue-700">Inicio</nuxt-link>
-                    <nuxt-link to="/blog" class="py-2 px-6 flex hover:text-blue-700">Blog</nuxt-link>
-                    <nuxt-link to="/libros" class="py-2 px-6 flex hover:text-blue-700">Libros</nuxt-link>
-                    <nuxt-link to="/materiales" class="py-2 px-6 flex hover:text-blue-700">Materiales</nuxt-link>
-                    <nuxt-link to="/actividades" class="py-2 px-6 flex hover:text-blue-700">Actividades</nuxt-link>
-                </nav>
-            </div>
+      <nav class="relative flex flex-wrap items-center justify-between px-2 py-3 bg-gray-900">
+        <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
+          <div class="w-full relative flex justify-between lg:w-auto px-4 lg:static lg:block lg:justify-start">
+          <h4 class="flex title-font font-medium text-white hover:text-blue-300 items-center mb-4 md:mb-0">
+            <span class="sm:text-2xl">Prof. Juan C. Reyes</span>
+          </h4>
+            <button class="text-3xl text-grye-900 cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-gray-50 rounded bg-white block lg:hidden outline-none focus:outline-none" type="button" v-on:click="toggleNavbar()">
+              <p class="fas fa-bars">&equiv;</p>
+            </button>
+          </div>
+          <div v-bind:class="{'hidden': !menuShow, 'flex': menuShow}" class="lg:flex lg:flex-grow items-center grid justify-items-center">
+            <ul class="flex flex-col lg:flex-row list-none justify-items-center lg:ml-auto">
+              <nuxt-link to="/" class="py-2 px-6 flex text-white hover:text-blue-300 justify-items-center">Inicio</nuxt-link>
+              <nuxt-link to="/blog" class="py-2 px-6 flex text-white hover:text-blue-300 justify-items-center" >Blog</nuxt-link>
+              <nuxt-link to="/libros" class="py-2 px-6 flex text-white hover:text-blue-300 justify-items-center">Libros</nuxt-link>
+              <nuxt-link to="/materiales" class="py-2 px-6 flex text-white hover:text-blue-300 justify-items-center">Materiales</nuxt-link>
+              <nuxt-link to="/actividades" class="py-2 px-6 flex text-white hover:text-blue-300 justify-items-center">Actividades</nuxt-link>
+            </ul>
+          </div>
         </div>
-    </header>
+      </nav>
 </template>
+
+<script>
+export default {
+  name: "pink-menu",
+  data() {
+    return {
+      menuShow: false
+    }
+  },
+  methods: {
+    toggleNavbar: function(){
+      this.menuShow = !this.menuShow;
+    }
+  }
+}
+</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,11 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900'
+      }
     ]
   },
 
@@ -34,6 +38,7 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
+    '@nuxtjs/google-fonts'
   ],
 
    // Modules (https://go.nuxtjs.dev/config-modules)
